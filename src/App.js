ad111c9e2f560962eba21ce6022b71500e2ed5cb
Kiya Thurman <kiya@unitedpixelpushers.com>
2019-06-26 19:02:15 -0500

    app source code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Button from '@material-ui/core/Button';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const BASE_URL = "https://thedigitalgurl.api-us1.com/api/3/contacts/";
+const API_KEY = `${process.env.REACT_APP_CONTACTS_API_KEY}`;
+const CORS = `${'https://cors-anywhere.herokuapp.com/'}`;
+
+
+class ContactList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      error: null,
+      isLoaded: false
+    }
+  }
+  componentDidMount() {
+    this.setState({ isLoading: true })
+    axios.get(CORS + BASE_URL, {
+      headers: { 'Api-Token': API_KEY }
+    })
+      .then(response => {
+        if (response.status === 200 && response != null) {
+          this.setState({
+            data: response.data.contacts
+          });
+          console.log(response);
+        } else {
+          console.log('problem');
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      });
+
+  }
+  render() {
+    const { data } = this.state;
+    return (
+      <div className="wrapper">
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell align="left" className="name">Full Name</TableCell>
+              <TableCell align="left" className="email">Email</TableCell>
+              <TableCell align="center" className="phone">Phone</TableCell>
+              <TableCell align="center" className="org">Organization</TableCell>
+              <TableCell align="center" className="dateCreated">Date Ceated</TableCell>
+              <TableCell align="center" className="sendEmail"></TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data.map(item => (
+              <TableRow key={item.id}>
+                <TableCell align="left" className="name">{item.firstName} {item.lastName}</TableCell>
+                <TableCell align="left" className="email">{item.email}</TableCell>
+                <TableCell align="center" className="phone">{item.phone}</TableCell>
+                <TableCell align="center" className="org">{item.orgname}</TableCell>
+                <TableCell align="center" className="dateCreated">{item.cdate}</TableCell>
+                <TableCell align="center" className="sendEmail">
+                  <Button variant="outlined" className="button"> Email</Button>
+                </TableCell>
+              </TableRow >
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+    )
+  }
 }
-
-export default App;
+export default ContactList;
